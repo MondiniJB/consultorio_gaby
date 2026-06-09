b1673112274aeb96de6c94c34fcc60c70cd4f533
Qvanta Studio <hello@qvantastudio.com>
2026-06-09 00:08:05 -0300

    feat: integrate Lenis smooth scrolling and Tailwind CSS base styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,39 @@
 <script setup>
+import { onMounted, onUnmounted } from 'vue'
+import Lenis from 'lenis'
 import HomeView from './views/HomeView.vue'
 import FloatingWhatsapp from './components/layout/FloatingWhatsapp.vue'
+
+let lenis
+let rafId
+
+onMounted(() => {
+  lenis = new Lenis({
+    duration: 1.5,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    orientation: 'vertical',
+    gestureOrientation: 'vertical',
+    smoothWheel: true,
+    wheelMultiplier: 1,
+    infinite: false,
+  })
+
+  function raf(time) {
+    lenis.raf(time)
+    rafId = requestAnimationFrame(raf)
+  }
+
+  rafId = requestAnimationFrame(raf)
+})
+
+onUnmounted(() => {
+  if (lenis) {
+    lenis.destroy()
+  }
+  if (rafId) {
+    cancelAnimationFrame(rafId)
+  }
+})
 </script>
 
 <template>
